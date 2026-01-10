f1a2f7cde364a335389172c8940d106c05f0b360
Hyrum Whittier <whittierht@gmail.com>
2026-01-10 11:08:07 -0700

    Prep1 and Prep2!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        string letter;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Great Job! You passed the course, go get some Kiwi Loco to celebrate!");
+        }
+        else
+        {
+            Console.WriteLine("Welp, there is always next semester...");
+        }
     }
 }
