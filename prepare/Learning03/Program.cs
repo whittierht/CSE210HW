600c950da8b47aa02a6f9f7dd054c760a661d9ed
Hyrum Whittier <whittierht@gmail.com>
2026-02-05 18:10:15 -0700

    Learning 3 encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+
+        Fraction second = new Fraction(5);
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+
+        Fraction third = new Fraction(3, 4);
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
+        Fraction fourth = new Fraction(1, 3);
+        Console.WriteLine(fourth.GetFractionString());
+        Console.WriteLine(fourth.GetDecimalValue());
+
+        Fraction practice = new Fraction();
+        Random random = new Random();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            int top = random.Next(1, 11);
+            int bottom = random.Next(1, 11);
+
+            practice.SetTop(top);
+            practice.SetBottom(bottom);
+
+            Console.WriteLine(
+                "Fraction " + i +
+                ": string: " + practice.GetFractionString() +
+                " Number: " + practice.GetDecimalValue()
+            );
+        }
     }
 }
