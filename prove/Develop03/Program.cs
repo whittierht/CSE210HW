33f43b59cde62cd83cfed80c80b2e6dc049153eb
Hyrum Whittier <whittierht@gmail.com>
2026-02-12 18:05:22 -0700

    Prgram and Reference login!

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,26 +1,25 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("");
-        Verse verse = new Verse(reference, "");
+        Reference reference = new Reference("John", 3, 16);
+        Verse verse = new Verse(reference, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
 
-        while (!verse.IsCompletelyHidden())
+        string input = "";
+
+        while (input != "quit" && !verse.IsCompletelyHidden())
         {
             Console.Clear();
             Console.WriteLine(verse.GetText());
-            Console.WriteLine("Press Enter to continue or type quit: ");
+            Console.WriteLine();
+            Console.Write("Press Enter to continue or type quit: ");
 
-            string input = Console.ReadLine();
+            input = Console.ReadLine();
 
-            if (input == "quit")
+            if (input != "quit")
             {
-                break;
+                verse.HideRandomWords(3);
             }
-
-            verse.HideRandomWords(3);
         }
     }
 }
