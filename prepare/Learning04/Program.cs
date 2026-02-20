50b74d12dd3e415842dc4f23bcd51822476764f8
Hyrum Whittier <whittierht@gmail.com>
2026-02-19 20:02:51 -0700

    cool stuff

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        var assignment = new Assignment("Hyrum Whittier", "Creative Writing");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+
+
+        var math = new MathAssignment("Kari Whittier", "Algebra", "5.2", "1-15, 18-20");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+
+
+        var writing = new WritingAssignment("Danny Star", "Modern Literature", "The Evolution of Storytelling");
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+        Console.WriteLine();
+
+
+
+        var randomMath = new MathAssignment("Hyrum Whittier", "Geometry", "9.4", "3-12");
+
+        Console.WriteLine(randomMath.GetSummary());
+        Console.WriteLine(randomMath.GetHomeworkList());
+        Console.WriteLine();
     }
 }
