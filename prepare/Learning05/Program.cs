cf547e452ed95a95775b611f43099338624e25b9
Hyrum Whittier <whittierht@gmail.com>
2026-03-05 18:02:28 -0700

    shapes thiing polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        
+        shapes.Add(new Circle("Green", 3));
+
+
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"{s.GetColor()} area: {s.GetArea()}");
+        }
     }
 }
